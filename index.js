cafd1fa8f47bf0d58ee2eb3b73bdc143fa1c4fd0
haleybraker <haleybraker@gmail.com>
2018-07-13 16:10:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file.
+var names = ['Adele','Beyonce','Lady','Madonna','Rihanna','Taylor'];
+
+function lowerCaseStudentNames(name) {
+  //let lowerCaseNames = names.map
+  //return lowerCaseNames.toLowerCase()
+  //let lower = names.map(lowerCaseStudentNames)
+  //console.log(`${names.toLowerCase()}`)
+}
